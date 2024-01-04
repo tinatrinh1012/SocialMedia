19cfa3ffb7d5c72ab07f6b4b86c5cbc619e14771
tinatrinh1012 <68746212+tinatrinh1012@users.noreply.github.com>
2024-01-03 20:56:47 -0600

    root route and error page

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,14 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import ErrorPage from './pages/error-page';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById('root') as HTMLElement
 );
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+		errorElement: <ErrorPage />
+	}
+])
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router}></RouterProvider>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
