356b163ce4b6f952a7d1f41cfb158075e0aea7c5
tinatrinh1012 <68746212+tinatrinh1012@users.noreply.github.com>
2024-03-14 19:44:26 -0500

    refactor get news feed post

--- client/src/pages/news-feed-page.tsx
+++ client/src/pages/news-feed-page.tsx
@@ -1,24 +1,9 @@
-import { useEffect, useState } from "react"
 import { PostModel } from "../models/post";
 import Post from "../components/post";
+import { useGet } from "../hooks/useGet";
 
 export default function NewsFeedPage() {
-    const [feedPosts, setFeedPosts] = useState<PostModel[]>();
-
-    useEffect(() => {
-        async function getUserNewsFeed() {
-            try {
-                const response = await fetch('http://localhost:3000/posts/news-feed', { credentials: 'include'});
-                const posts = await response.json();
-                setFeedPosts(posts);
-            } catch (error) {
-
-            }
-
-        }
-
-        getUserNewsFeed();
-    }, [])
+    const { data: feedPosts } = useGet<PostModel[]>('/posts/news-feed');
 
     return (
         <>
